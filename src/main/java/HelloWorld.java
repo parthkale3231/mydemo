d372381dfa88b032ad1ea3c43f2a1b482c72f21f
IF- Parth kale <parthkale3231@gmail.com>
2026-02-05 19:38:36 +0530

    simple maven java project  for addition

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins with Maven!");
+         int a=10;
+         int b=20;
+            int sum=a+b;
+            System.out.println(sum);    
     }
 }
